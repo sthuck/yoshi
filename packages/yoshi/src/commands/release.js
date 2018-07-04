043e87e35ca5f0b6318bff0f65d06a798e945104
Ran Yitzhaki <ranyitz@gmail.com>
2018-07-04 16:30:12 +0300

    update wnpm-ci to version 7

--- packages/yoshi/src/commands/release.js
+++ packages/yoshi/src/commands/release.js
@@ -6,14 +6,4 @@ const runner = createRunner({
   logger: new LoggerPlugin(),
 });
 
-module.exports = runner.command(async () => {
-  return new Promise((resolve, reject) => {
-    return wnpm.prepareForRelease({ shouldShrinkWrap: false }, err => {
-      if (err) {
-        return reject(err);
-      }
-
-      return resolve();
-    });
-  });
-});
+module.exports = runner.command(async () => wnpm.prepareForRelease());
