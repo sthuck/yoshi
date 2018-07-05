22d1b02390e2a8efe0da3c50d57b2beb01a64bb7
Ronen Amiel <ronen.amiel@gmail.com>
2018-07-05 18:40:00 +0300

    add a way to configure puppeteer

--- packages/jest-yoshi-fullstack-preset/globalSetup.js
+++ packages/jest-yoshi-fullstack-preset/globalSetup.js
@@ -5,10 +5,16 @@ const puppeteer = require('puppeteer');
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
+const config = require(path.join(process.cwd(), 'jest-yoshi.config.js'));
+
 module.exports = async () => {
   const browser = (global.BROWSER = await puppeteer.launch({
+    // defaults
     headless: true,
     args: ['--no-sandbox'],
+
+    // user defined options
+    ...config.puppeteer,
   }));
 
   await fs.outputFile(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint());
